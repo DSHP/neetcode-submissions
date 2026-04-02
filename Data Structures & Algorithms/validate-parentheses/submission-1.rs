5efd84b4bd9572ee39e3a84da8965d569069bd14
Daniel Sjö <DSHP@users.noreply.github.com>
2026-04-02 23:05:14 +0700

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.rs
@@ -0,0 +1,21 @@
+impl Solution {
+    pub fn is_valid(s: String) -> bool {
+        let mut stack: Vec<char> = Vec::new();
+
+        for c in s.chars() {
+            match c {
+                '(' => stack.push(')'),
+                '{' => stack.push('}'),
+                '[' => stack.push(']'),
+                ')' | '}' | ']' => {
+                    if stack.pop() != Some(c) {
+                        return false;
+                    }
+                }
+                _ => continue,
+            }
+        }
+
+        stack.is_empty()
+    }
+}
